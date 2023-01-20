946d5b7850407c5e0e49e9df0f6126c010abc05a
João Vitor <joaovitornascimento@gmail.com>
2023-01-19 21:09:38 -0300

    adding updates and save modules in browser

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 const form = document.querySelector(`#form-habits`)
 const nlwSetup = new NLWSetup(form)
+// Selecionando o botão dentro do header
+const button = document.querySelector(`header button`)
 
-const data = {
-  run: [`01-01`, `01-02`, `01-03`, `01-04`, `01-05`, `01-06`, `01-07`, `01-08`],
+// Function após o click
+button.addEventListener(`click`, add)
+// Todas as vezes que o formullário for alterado, irá salvar as informações no navegador
+form.addEventListener(`change`, save)
+
+function add () {
+  // Criando ma nova data, transformando-a em data brasileira e excluindo os números de ano 
+  const today = new Date().toLocaleDateString(`pt-br`).slice(0, -5)
+  // Verificando se o dia já existe
+  const dayExists = nlwSetup.dayExists(today)
+
+  // Se o dia já existir, não vai adiciona-lo mais
+  if (dayExists === true) {
+    alert(`Dia incluso`)
+  } 
+  // Se não existir, adicione-o
+  else {
+    nlwSetup.addDay(today) 
+  }
 }
 
+// Todas as vezes que o formullário for alterado, irá salvar as informações no navegador
+function save () {
+  localStorage.setItem(`save habits`, JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem(`save habits`)) || {}
 nlwSetup.setData(data)
-nlwSetup.load()
+ nlwSetup.load()
